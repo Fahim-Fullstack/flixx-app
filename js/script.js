9892216c3a8f56b4bdd5aef944f1d5460f5e729c
Fahim Hasan <71988931+Fahim-Fullstack@users.noreply.github.com>
2023-10-01 20:01:21 -0500

    adding new

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ const global = {
 };
 
 //Display movie details
-
 async function displayMovieDetails() {
   const movieID = window.location.search.split('=')[1];
   console.log(movieID); //'?id=565770'
@@ -15,54 +14,6 @@ async function displayMovieDetails() {
   const movie = await movieFetchAPI(`movie/${movieID}`);
 
   const div = document.createElement('div');
-
-  div.innerHTML = `  <div class="details-top">
-  <div>
-  ${
-    movie.poster_path
-      ? `<img
-  src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-  class="card-img-top"
-  alt= "${movie.title}"
-/>`
-      : `<img
-src="images/no-image.jpg"
-class="card-img-top"
-alt= "Movie Title"
-/>`
-  }
-  </div>
-  <div>
-    <h2>${movie.title}</h2>
-    <p>
-      <i class="fas fa-star text-primary"></i>
-      ${movie.vote_average.toFixed(1)} / 10
-    </p>
-    <p class="text-muted">Release Date: ${movie.release_date}</p>
-    <p>
-      ${movie.overview}
-    </p>
-    <h5>Genres</h5>
-    <ul class="list-group">
-    ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
-
-    </ul>
-    <a href="${
-      movie.homepage
-    }" target="_blank" class="btn">Visit Movie Homepage</a>
-  </div>
-</div>
-<div class="details-bottom">
-  <h2>Movie Info</h2>
-  <ul>
-    <li><span class="text-secondary">Budget:</span>$${movie.budget}</li>
-    <li><span class="text-secondary">Revenue:</span> $2,000,000</li>
-    <li><span class="text-secondary">Runtime:</span> 90 minutes</li>
-    <li><span class="text-secondary">Status:</span> Released</li>
-  </ul>
-  <h4>Production Companies</h4>
-  <div class="list-group">Company 1, Company 2, Company 3</div>
-</div>`;
 
   document.querySelector('#movie-details').appendChild(div);
 }
@@ -75,7 +26,7 @@ async function movieFetchAPI(endpoint) {
   showSpinner();
 
   const response = await fetch(
-    `${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`,
+    `${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`
   );
 
   const data = await response.json();
