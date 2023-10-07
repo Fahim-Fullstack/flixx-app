3977e19508addb7207109e1b8efb64ed92b373f3
Fahim Hasan <71988931+Fahim-Fullstack@users.noreply.github.com>
2023-10-07 07:45:57 -0500

    search bar

--- js/script.js
+++ js/script.js
@@ -27,11 +27,51 @@ async function search() {
 
   if (global.search.term !== '' && global.search.term !== null) {
     // make request and display results
-    const results = await searchApiData();
-    console.log(results);
+    const { results, total_pages, page } = await searchApiData();
+
+    if (results.length === 0) {
+      showAlert('No results found');
+      return;
+    }
+
+    displaySearchResults(results);
+
+    //clear the input
+    document.querySelector('#search-term').value = '';
   } else {
     showAlert('Please enter a search term');
   }
+}
+//Display Search Results
+function displaySearchResults(results) {
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+
+    div.innerHTML = `<a href="tv-details.html?id=${show.id}">
+    ${
+      show.poster_path
+        ? `<img
+    src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+    class="card-img-top"
+    alt= "${show.name}show.name
+  />`
+        : `<img
+  src="images/no-image.jpg"
+  class="card-img-top"
+  alt= "Movie Title"
+/>`
+    }
+  </a>
+  <div class="card-body">
+    <h5 class="card-title">${show.name}</h5>
+    <p class="card-text">
+      <small class="text-muted">Release: ${show.first_air_date}</small>
+    </p>
+  </div>`;
+
+    document.querySelector('#popular-shows').appendChild(div);
+  });
 }
 
 // Display Slider Movies
@@ -288,7 +328,7 @@ function highlightActiveLink() {
 }
 
 // Show Error Alert
-function showAlert(message, className) {
+function showAlert(message, className = 'error') {
   const alertEl = document.createElement('div');
   alertEl.classList.add('alert', className);
   alertEl.appendChild(document.createTextNode(message));
